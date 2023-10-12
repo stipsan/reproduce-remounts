6a230cd29ba4855e5f1f9229ecfce8106b10a500
jfulse <jorgen.fulsebakke@protonmail.com>
2023-10-12 11:27:22 +0200

    Reproduce remounts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { LiveQueryProvider } from "@sanity/preview-kit";
+import { client } from "../../sanity/lib/client";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <LiveQueryProvider client={client}>
+      <Component {...pageProps} />
+    </LiveQueryProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    console.log("💥 MOUNT HOME 💥");
+  }, []);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      TODO
+      HOME
     </main>
   );
 }
